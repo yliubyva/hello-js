58051bc8de4c3882b74f7691858e96f8a45a8796
yliubyva <yliubyva@gmail.com>
2024-08-06 18:34:22 +0300

    fixed variables

--- task-max.js
+++ task-max.js
@@ -1,26 +1,19 @@
 'use strict';
 //Max 1
 const pi = Math.PI;
-let r; //радіус
-let S; //площа
-
-r = prompt(`Введіть радіус кола`);
-S = pi * r ** 2;
-alert(`Площа круга: ${S.toFixed(2)}`);
+const radius = prompt(`Введіть радіус кола`);
+const area = pi * radius ** 2;
+alert(`Площа круга: ${area.toFixed(2)}`);
 
 //Max 2
-let v; //швидкість
-let d; //відстань
-let t; //час 
-
-d = prompt(`Яка відстань в кілометрах між двома містами?`);
-t = prompt(`За скільки годин Ви хочете дістатись?`);
-v = d / t;
-alert(`Щоб встигнути вчасно, Вам необхідно рухатись зі швидкістью ${v.toFixed(2)} км/год`);
+const distance = prompt(`Яка відстань в кілометрах між двома містами?`);
+const time = prompt(`За скільки годин Ви хочете дістатись?`);
+const speed = distance / time;
+alert(`Щоб встигнути вчасно, Вам необхідно рухатись зі швидкістью ${speed.toFixed(2)} км/год`);
 
 //Max 3
 const EUR = 0.92;
 
-let userMoneyU = prompt(`Введіть необхідну суму USD, яку ви хочете обміняти:`);
-let userMoneyE = userMoneyU * EUR;
-alert(`Ваша сума до отримання: ${userMoneyE.toFixed(2)} EUR`);
+const moneyInUSD = Number(prompt(`Введіть необхідну суму USD, яку ви хочете обміняти:`));
+const moneyInEUR = moneyInUSD * EUR;
+alert(`Ваша сума до отримання: ${moneyInEUR.toFixed(2)} EUR`);
--- task-norm.js
+++ task-norm.js
@@ -2,18 +2,14 @@
 
 const currentYear = 2024;
 const sidesQuantity = 4;
-let userName;
-let userYearOfBirth;
-let length;
-
 //normal 1
-userName = prompt(`Ваше ім'я?`);
+const userName = prompt(`Ваше ім'я?`);
 alert(`Привіт, ${userName}`);
 
 //normal 2
-userYearOfBirth = prompt(`Ваш рік народження:`);
+const userYearOfBirth = prompt(`Ваш рік народження:`);
 alert(`Виходить Вам аж ${currentYear - userYearOfBirth}!`);
 
 //normal 3
-length = prompt(`Що ж, я допоможу тоді Вам знайти периметр квадрату! Підкажіть довжину однієї сторони?`);
+const length = prompt(`Що ж, я допоможу тоді Вам знайти периметр квадрату! Підкажіть довжину однієї сторони?`);
 alert(`Периметр цього квадрату ${sidesQuantity * length}`);
